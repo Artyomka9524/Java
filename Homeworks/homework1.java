d017cab4344beeb3e941659cdb78053869dc0856
Artyom <csgo1320@gmail.com>
2022-11-26 22:00:44 +0300

    Task4

--- Homeworks/homework1.java
+++ Homeworks/homework1.java
@@ -50,3 +50,25 @@ class Solution {
     }
 }
 
+// Task4
+// http://joxi.ru/a2XgDYjuQ6g01m
+
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder(words.length);
+        for (int i = words.length - 1; i >= 0; i--) {
+            if (words[i].length() != 0) {
+                if (result.length() != 0) {
+
+                    result.append(" ");
+                }
+
+                result.append(words[i]);
+            }
+        }
+
+        return result.toString();
+    }
+}
+
