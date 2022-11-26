1d22c5cef3f1bb1bfb2d16b9a349a53073bc3bf8
Artyom <csgo1320@gmail.com>
2022-11-26 22:30:22 +0300

    Task5

--- Homeworks/homework1.java
+++ Homeworks/homework1.java
@@ -72,3 +72,24 @@ class Solution {
     }
 }
 
+// Task 5
+// http://joxi.ru/GrqDKB8czZxNDA
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
+                ++i;
+            } else if (!Character.isLetterOrDigit(s.charAt(j))) {
+                --j;
+            } else if (Character.toUpperCase(s.charAt(i)) != Character.toUpperCase(s.charAt(j))) {
+                return false;
+            } else {
+                ++i;
+                --j;
+            }
+        }
+        return true;
+    }
+}
