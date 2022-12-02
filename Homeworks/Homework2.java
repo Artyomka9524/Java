9a26251b033d264cdcbcf9fa98375f7b73ca59b9
Artyom <csgo1320@gmail.com>
2022-12-03 02:38:37 +0300

    Task4

--- Homeworks/Homework2.java
+++ Homeworks/Homework2.java
@@ -71,7 +71,7 @@ class Solution {
     }
 }
 
-//Ефыл3
+//Task3
 //http://joxi.ru/1A501JEH4M98N2
 
 /**
@@ -113,3 +113,62 @@ class Solution {
             t.remove(t.size() - 1);
         }
     }
+
+    //Task4
+    //http://joxi.ru/l2ZGMYzilo0j0A
+
+      /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p == q) {
+                return true;
+            }
+            if (p == null || q == null) {
+                return false;
+            }
+            Deque<TreeNode> q1 = new ArrayDeque<>();
+            Deque<TreeNode> q2 = new ArrayDeque<>();
+            q1.offer(p);
+            q2.offer(q);
+            while (!q1.isEmpty() && !q2.isEmpty()) {
+                p = q1.poll();
+                q = q2.poll();
+                if (p.val != q.val) {
+                    return false;
+                }
+                TreeNode la = p.left, ra = p.right;
+                TreeNode lb = q.left, rb = q.right;
+                if ((la != null && lb == null) || (lb != null && la == null)) {
+                    return false;
+                }
+                if ((ra != null && rb == null) || (rb != null && ra == null)) {
+                    return false;
+                }
+                if (la != null) {
+                    q1.offer(la);
+                    q2.offer(lb);
+                }
+                if (ra != null) {
+                    q1.offer(ra);
+                    q2.offer(rb);
+                }
+            }
+            return true;
+        }
+    }
+
+
