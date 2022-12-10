2c1349219b52e242d456217159e1635a59207755
Artyom <csgo1320@gmail.com>
2022-12-11 02:05:37 +0300

    2

--- Homeworks/Homework5.java
+++ Homeworks/Homework5.java
@@ -22,3 +22,36 @@ class Solution {
         return false;
     }
 }
+
+//Task2
+//http://joxi.ru/MAjDJencdMMYDm
+
+class Solution {
+    public int countWords(String[] words1, String[] words2) {
+        
+        Map<String, int[]> common = new HashMap<>();
+        int count = 0;
+        
+        for(int i = 0; i < words1.length; i++) {
+            String word = words1[i];
+            int[] counter = common.getOrDefault(word, new int[]{0, 0});
+            counter[0]++;
+            common.put(word, counter);
+        }
+        
+        for(int i = 0; i < words2.length; i++) {
+            String word = words2[i];
+            int[] counter = common.getOrDefault(word, null);
+            if(counter != null) {
+                if(counter[0] == 1 && counter[1] == 0) {
+                    count++;
+                } else if(counter[0] == 1 && counter[1] == 1) {
+                    count--;
+                }
+                counter[1]++;
+            }
+        }
+        
+        return count;
+    }
+}
